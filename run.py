07bb698e23bbbd07e34f0b3f3d7f280c8c42d71f
ThomasSpieler <155924976+ThomasSpieler@users.noreply.github.com>
2026-05-25 17:58:31 -0700

    Separated run.py and uno.py

---
+++ run.py
@@ -0,0 +1,100 @@
+from dataclasses import replace
+from random import choice
+import uno
+
+def simulate_game(debug: bool = False) -> tuple[str, int]:
+    players: list[uno.Player] = [uno.Player(name) for name in ["Player 1", "Player 2", "Player 3", "Player 4"]]
+
+    deck = uno.Deck()
+    deck.shuffle()
+
+    turn: int = 0
+    turn_counter: int = 0
+    direction: int = 1
+
+    for _ in range(0, 7):
+        for player in players:
+            player.draw_card(deck.draw())
+            
+    def next_player(turn: int) -> int:
+        turn += direction
+        if turn > players.__len__() - 1:
+            turn = 0
+            
+        if turn < 0:
+            turn = players.__len__() - 1
+            
+        return turn
+
+    while True:
+        if turn_counter > 10000:
+            return ("Timed Out", turn_counter)
+        
+        current_player = players[turn]
+        card_played = False
+
+        for card in current_player.hand:
+            if deck.can_play_card(card):
+                current_player.play_card(card, deck)
+                card_played = True
+                
+                if debug:
+                    print(f"{current_player.name} plays {str(card)}.")
+                
+                if card.value == uno.Value.REVERSE:
+                    direction *= -1
+                    
+                if card.value == uno.Value.SKIP:
+                    turn = next_player(turn)
+                    
+                if card.value == uno.Value.DRAW_TWO:
+                    next = next_player(turn)
+                    for i in range(0, 2):
+                        players[next].draw_card(deck.draw())
+                    turn = next_player(next)
+                    
+                if card.value in [uno.Value.WILD, uno.Value.DRAW_FOUR]:
+                    new_color = choice([uno.Color.RED, uno.Color.YELLOW, uno.Color.GREEN, uno.Color.BLUE])
+                    deck.discard[0] = replace(deck.discard[0], color=new_color)
+                    
+                    if debug:
+                        print(f"{current_player.name} changes color to {new_color.value}.")
+                
+                if card.value == uno.Value.DRAW_FOUR:
+                    next = next_player(turn)
+                    for i in range(0, 4):
+                        players[next].draw_card(deck.draw())
+                    turn = next_player(next)
+                    
+                break
+            
+        if current_player.hand.__len__() == 0:
+            if debug:
+                print(f"{current_player.name} wins!")
+                print(f"Game ended in {turn_counter} turns.")
+                
+            return (current_player.name, turn_counter)
+            
+        if not card_played:
+            if debug:
+                print(f"{current_player.name} draws.")
+                
+            current_player.draw_card(deck.draw())
+            
+        current_player.shuffle_hand()
+        turn = next_player(turn)
+        turn_counter += 1
+        
+if __name__ == "__main__":
+    game_status: list[tuple[str, int]] = []
+    for i in range(0, 1000):
+        game_status.append(simulate_game())
+        
+    print("Player Wins:\n")
+
+    player_wins = {}
+    for status in game_status:
+        player_wins[status[0]] = player_wins.get(status[0], 0) + 1
+        
+    for player, wins in player_wins.items():
+        print(f"{player}: {wins}")
--- uno.py
+++ uno.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass, replace
+from dataclasses import dataclass
 from enum import Enum
-from random import choice, shuffle
+from random import shuffle
 
 class Color(Enum):
     RED = "Red"
@@ -133,99 +133,3 @@ class Player:
             deck.discard.insert(0, card)
             
         return playable
-
-def simulate_game(debug: bool = False) -> tuple[str, int]:
-    players: list[Player] = [Player(name) for name in ["Player 1", "Player 2", "Player 3", "Player 4"]]
-
-    deck = Deck()
-    deck.shuffle()
-
-    turn: int = 0
-    turn_counter: int = 0
-    direction: int = 1
-
-    for _ in range(0, 7):
-        for player in players:
-            player.draw_card(deck.draw())
-            
-    def next_player(turn: int) -> int:
-        turn += direction
-        if turn > players.__len__() - 1:
-            turn = 0
-            
-        if turn < 0:
-            turn = players.__len__() - 1
-            
-        return turn
-
-    while True:
-        if turn_counter > 10000:
-            return ["Timed Out", turn_counter]
-        
-        current_player = players[turn]
-        card_played = False
-
-        for card in current_player.hand:
-            if deck.can_play_card(card):
-                current_player.play_card(card, deck)
-                card_played = True
-                
-                if debug:
-                    print(f"{current_player.name} plays {str(card)}.")
-                
-                if card.value == Value.REVERSE:
-                    direction *= -1
-                    
-                if card.value == Value.SKIP:
-                    turn = next_player(turn)
-                    
-                if card.value == Value.DRAW_TWO:
-                    next = next_player(turn)
-                    for i in range(0, 2):
-                        players[next].draw_card(deck.draw())
-                    turn = next_player(next)
-                    
-                if card.value in [Value.WILD, Value.DRAW_FOUR]:
-                    new_color = choice([Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE])
-                    deck.discard[0] = replace(deck.discard[0], color=new_color)
-                    
-                    if debug:
-                        print(f"{current_player.name} changes color to {new_color.value}.")
-                
-                if card.value == Value.DRAW_FOUR:
-                    next = next_player(turn)
-                    for i in range(0, 4):
-                        players[next].draw_card(deck.draw())
-                    turn = next_player(next)
-                    
-                break
-            
-        if current_player.hand.__len__() == 0:
-            if debug:
-                print(f"{current_player.name} wins!")
-                print(f"Game ended in {turn_counter} turns.")
-                
-            return [current_player.name, turn_counter]
-            
-        if not card_played:
-            if debug:
-                print(f"{current_player.name} draws.")
-                
-            current_player.draw_card(deck.draw())
-            
-        current_player.shuffle_hand()
-        turn = next_player(turn)
-        turn_counter += 1
-        
-game_status: list[tuple[str, int]] = []
-for i in range(0, 1000):
-    game_status.append(simulate_game())
-    
-print("Player Wins:\n")
-
-player_wins = {}
-for status in game_status:
-    player_wins[status[0]] = player_wins.get(status[0], 0) + 1
-    
-for player, wins in player_wins.items():
-    print(f"{player}: {wins}")
