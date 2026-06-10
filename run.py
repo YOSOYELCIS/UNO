a44c1186c5d277267a55bc8215f5b5d451605112
ThomasSpieler <155924976+ThomasSpieler@users.noreply.github.com>
2026-06-09 18:36:54 -0700

    Expanded game state class and moved misplaced run.py functions to uno.py

--- run.py
+++ run.py
@@ -1,85 +1,25 @@
-from dataclasses import replace
-from random import choice
 import uno
 import simple_agents
+#import mdp_agent
 
 def simulate_game(players: list[uno.Player] = 
                   [simple_agents.Randy("Player 1"), simple_agents.Randy("Player 2"), simple_agents.Randy("Player 3"), simple_agents.Randy("Player 4")],
                   debug: bool = False) -> tuple[str, int]:
-    player_count = len(players)
-    deck = uno.Deck()
-    deck.shuffle()
-
-    turn: int = 0
-    turn_counter: int = 0
-    direction: int = 1
-
-    for _ in range(0, 7):
-        for player in players:
-            player.draw_card(deck.draw())
             
-    def next_player(turn: int) -> int:
-        turn += direction
-        if turn > players.__len__() - 1:
-            turn = 0
-            
-        if turn < 0:
-            turn = players.__len__() - 1
-            
-        return turn
+    game_state: uno.GameState = uno.GameState(players, debug)
 
     while True:
-        if turn_counter > 10000:
-            return ("Timed Out", turn_counter)
+        if game_state.turn_counter > 10000:
+            return ("Timed Out", game_state.turn_counter)
         
-        current_player = players[turn]
-        card_played = current_player.play_card(uno.GameState(deck, player_count))
-
-        if type(card_played) == uno.Card:
-            deck.discard.insert(0, card_played)
-
-            if debug:
-                print(f"{current_player.name} plays {str(card_played)}.")
+        current_player: uno.Player = game_state.process_turn()
             
-            match card_played.value:
-                case uno.Value.REVERSE:
-                    direction *= -1
-                
-                case uno.Value.SKIP:
-                    turn = next_player(turn)
-                
-                case uno.Value.DRAW_TWO:
-                    next = next_player(turn)
-                    for i in range(2):
-                        players[next].draw_card(deck.draw())
-                    turn = next_player(next)
-                
-                case x if x in [uno.Value.WILD, uno.Value.DRAW_FOUR]:
-                    new_color = current_player.choose_color()
-                    deck.discard[0] = replace(deck.discard[0], color=new_color)
-                    if debug:
-                        print(f"{current_player.name} changes color to {new_color.value}.")
-                    if card_played == uno.Value.DRAW_FOUR:
-                        next = next_player(turn)
-                        for i in range(0, 4):
-                            players[next].draw_card(deck.draw())
-                        turn = next_player(next)
-            
-        if current_player.hand.__len__() == 0:
+        if game_state.game_end:
             if debug:
                 print(f"{current_player.name} wins!")
-                print(f"Game ended in {turn_counter} turns.")
+                print(f"Game ended in {game_state.turn_counter} turns.")
                 
-            return (current_player.name, turn_counter)
-            
-        if card_played == False:
-            if debug:
-                print(f"{current_player.name} draws.")
-                
-            current_player.draw_card(deck.draw())
-            
-        turn = next_player(turn)
-        turn_counter += 1
+            return (current_player.name, game_state.turn_counter)
         
 # simulate_many_games() : Runs "n" games of UNO using simulate_game()
 def simulate_many_games(n: int, players: list[uno.Player], debug: bool = False) -> None:
