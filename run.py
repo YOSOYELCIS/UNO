ef0f44cd234db1800927a6874253d8ce3f5e4ae7
Cis Garcia <yosoyelcis@duck.com>
2026-06-10 20:34:42 -0700

    Simple expectimax
    With 2 simple heuristics and a probabilistic agent. Wins about 30% against simple agents.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import agents.expectimax as exp_agent
 import agents.mdp_agent as mdp_agent
 import agents.simple_agents as simple_agents
 import uno
@@ -38,15 +39,18 @@ def simulate_many_games(n: int, players: list[uno.Player], debug: bool = False) 
         player_wins[result[0]] = player_wins.get(result[0], 0) + 1
         
     # Sorts in descending order of wins.
+    max_ratio = 0
     win_counts = sorted(player_wins.items(), key=lambda element: element[1], reverse=True)
     for player, wins in win_counts:
+        max_ratio = wins / n
         print(f"{player}: {wins}")
+        print(f"Player {player} won: {max_ratio:.2%} of games")
 
 if __name__ == "__main__":
     players = [ 
         simple_agents.Default("Default 1"),
         simple_agents.Default("Default 2"),
         simple_agents.Default("Default 3"),
-        mdp_agent.MDPAgent("MDP")]
+        exp_agent.ExpectimaxAgent("Expert")]
     
     simulate_many_games(1, players)
